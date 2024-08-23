d99a092be147f4d8596b82853042ed20562d73cb
kylie <cooper.kylie1077@gmail.com>
2024-08-23 04:40:21 -0500

    -Hotfix KeyMapping server crash

--- src/main/java/com/dragn0007/dragnvehicles/registry/VehicleKeyMappings.java
+++ src/main/java/com/dragn0007/dragnvehicles/registry/VehicleKeyMappings.java
@@ -1,14 +1,18 @@
 package com.dragn0007.dragnvehicles.registry;
 
+import com.dragn0007.dragnvehicles.ValiantVehiclesMain;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.lwjgl.glfw.GLFW;
 
+@Mod.EventBusSubscriber(modid = ValiantVehiclesMain.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class VehicleKeyMappings {
     public static final KeyMapping DRIFT_KEY = new KeyMapping(
             "key.dragnvehicles.drift",
